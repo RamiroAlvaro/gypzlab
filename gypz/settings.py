2067c3d303f836a9acf493706270d99ce4d66280
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-12-04 16:58:16 -0300

    installation and configuration python-decouple

--- gypz/settings.py
+++ gypz/settings.py
@@ -10,9 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from functools import partial
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config, Csv
+from dj_database_url import parse as db_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$hy!e%qje!)n(!a1k84w(2ajw)utt$4!dc1u7k6$&6f6en1nwk'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
@@ -74,11 +78,10 @@ WSGI_APPLICATION = 'gypz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+parse_database = partial(db_url, conn_max_age=600)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database),
 }
 
 
