f40479fc02d5aecd34102735e1a5910fb41ebcf0
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-12-08 22:20:02 -0300

    More test

--- gypz/card/tests/test_card.py
+++ gypz/card/tests/test_card.py
@@ -1,4 +1,5 @@
 import pytest
+from django.urls import reverse
 from model_bakery import baker
 
 from gypz.card.models import Card
@@ -22,6 +23,11 @@ def client_logged_user(user, client):
     return client
 
 
+@pytest.fixture
+def resp(client_logged_user, db):
+    return client_logged_user.get(reverse('card-list'))
+
+
 def test_create_card_score(card):
     assert_equal(card.score, 299)
 
@@ -32,3 +38,37 @@ def test_create_card_credit(card):
 
 def test_create_card_solicitation_status(card):
     assert_false(card.solicitation_status)
+
+
+def test_status_code(resp):
+    assert_equal(resp.status_code, 200)
+
+
+def test_media_type(resp):
+    assert_equal(resp.accepted_media_type, 'application/json')
+
+
+@pytest.fixture
+def resp_post(client_logged_user, db):
+    card = {
+        'id': 2,
+        'user': 1,
+        'score': 337,
+        'credit': 1000.0,
+        'solicitation_status': True,
+        'solicitation_date': '2020-12-07'
+    }
+    return client_logged_user.post(reverse('card-list'), card)
+
+
+def test_status_code_post(resp_post):
+    assert_equal(resp_post.status_code, 201)
+
+
+@pytest.fixture
+def resp_anonymous_user(client, db):
+    return client.get(reverse('card-list'))
+
+
+def test_status_code_anonymous_user(resp_anonymous_user):
+    assert_equal(resp_anonymous_user.status_code, 403)
--- gypz/core/tests/test_core.py
+++ gypz/core/tests/test_core.py
@@ -1,4 +1,5 @@
 import pytest
+from django.urls import reverse
 from model_bakery import baker
 
 from gypz.django_assertions import assert_equal
@@ -10,5 +11,43 @@ def user(db, django_user_model):
     return user
 
 
+@pytest.fixture
+def client_logged_user(user, client):
+    client.force_login(user)
+    return client
+
+
+@pytest.fixture
+def resp(client_logged_user, db):
+    return client_logged_user.get(reverse('user-list'))
+
+
 def test_create_user(user):
     assert_equal(user.first_name, 'Ramiro')
+
+
+def test_status_code(resp):
+    assert_equal(resp.status_code, 200)
+
+
+def test_media_type(resp):
+    assert_equal(resp.accepted_media_type, 'application/json')
+
+
+@pytest.fixture
+def resp_post(client, db):
+    user = {
+        'id': 1,
+        'first_name': 'ramiro',
+        'last_name': 'alvaro',
+        'email': 'ramiroalvaro.ra@gmail.com',
+        'cpf': '700.673.051-13',
+        'phone': '31991387178',
+        'salary': 10000.0,
+        'birthdate': '1975-11-29'
+    }
+    return client.post(reverse('user-list'), user)
+
+
+def test_status_code_post(resp_post):
+    assert_equal(resp_post.status_code, 201)
